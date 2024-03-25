7035ffeea90cc4bbad52b068a18109ee80f6c4d8
jjunho50 <jjunho50@gmail.com>
2024-03-25 15:46:37 +0900

    adsf

---
+++ src/main/java/com/user/management/entity/Role.java
@@ -0,0 +1,27 @@
+package com.user.management.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String name;
+
+    @Builder
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/user/management/entity/User.java
@@ -0,0 +1,49 @@
+package com.user.management.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Users")
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private String id;
+
+    @Column(name = "user_name")
+    private String name; // 이름
+
+    @Column(name = "user_password")
+    private String password; // 비밀번호
+
+    @Column(name = "user_email")
+    private String email; // 이메일
+
+    @Column(name = "user_birth")
+    private String birth; // 생일(8자리)
+
+    @ManyToOne
+    @JoinColumn(name = "role_name")
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt; // 회원가입일자
+
+    @Column(name = "latest_login_at")
+    private LocalDateTime latestLoginAt; // 마지막 접속일
+
+    @Builder
+    public User(String id, String name, String password, String email, String birth, Role role, LocalDateTime createdAt, LocalDateTime latestLoginAt) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.birth = birth;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.latestLoginAt = latestLoginAt;
+    }
+}
