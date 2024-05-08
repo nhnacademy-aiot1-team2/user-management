d012203c61902a6748abfa86863d2330afff9727
{joohyeon98} <{ljh221803@gmail.com}>
2024-05-08 14:25:08 +0900

    Feat: 유효성 예외처리

--- src/main/java/com/user/management/advice/GlobalExceptionHandler.java
+++ src/main/java/com/user/management/advice/GlobalExceptionHandler.java
@@ -5,11 +5,14 @@ import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 모든 컨트롤러에서 발생하는 RuntimeException을 처리합니다.
@@ -17,8 +20,8 @@ import java.time.LocalDateTime;
  * @author jjunho50
  * @version 1.0.0
  */
-@ControllerAdvice
-@RestController
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     /**
@@ -46,5 +49,15 @@ public class GlobalExceptionHandler {
                 .body(errorDetails.toString(4));
     }
 
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
