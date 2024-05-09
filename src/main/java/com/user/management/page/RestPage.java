902a04d971402aeccb121ef5648ddcc4447c0eca
pass0210 <pass0210@gmail.com>
2024-05-09 14:01:47 +0900

    Refactor: RestPage JavaDoc 추가

--- src/main/java/com/user/management/page/RestPage.java
+++ src/main/java/com/user/management/page/RestPage.java
@@ -9,8 +9,23 @@ import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
+/**
+ * page 객체를 deserialization 하기 위한 Page 구현 클래스
+ *
+ * @param <T> page 내부 content에 담을 클래스
+ * @author parksangwon
+ * @version 1.0.0
+ */
 @JsonIgnoreProperties(ignoreUnknown = true, value = {"pageable"})
 public class RestPage<T> extends PageImpl<T> {
+    /**
+     * Instantiates a new Rest page.
+     *
+     * @param content the content
+     * @param page    the page
+     * @param size    the size
+     * @param total   the total
+     */
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public RestPage(@JsonProperty("content") List<T> content,
                     @JsonProperty("number") int page,
@@ -19,6 +34,11 @@ public class RestPage<T> extends PageImpl<T> {
         super(content, PageRequest.of(page, size), total);
     }
 
+    /**
+     * Instantiates a new Rest page.
+     *
+     * @param page the page
+     */
     public RestPage(Page<T> page) {
         super(page.getContent(), page.getPageable(), page.getTotalElements());
     }
