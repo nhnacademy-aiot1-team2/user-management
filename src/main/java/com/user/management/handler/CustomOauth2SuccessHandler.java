a1297ff51302481bdc31bfbb0f3eebc287027192
pass0210 <pass0210@gmail.com>
2024-06-01 17:43:40 +0900

    Docs: CustomOauth2SuccessHandler 수정
    JavaDoc 추가

--- src/main/java/com/user/management/handler/CustomOauth2SuccessHandler.java
+++ src/main/java/com/user/management/handler/CustomOauth2SuccessHandler.java
@@ -12,12 +12,17 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Oauth 로그인 성공시 필요한 작업을 수행하는 랜들러 클래
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 @Component
 @RequiredArgsConstructor
 public class CustomOauth2SuccessHandler implements AuthenticationSuccessHandler {
@@ -25,8 +30,16 @@ public class CustomOauth2SuccessHandler implements AuthenticationSuccessHandler 
     @Value("${front.url}")
     private String frontUrl;
 
+    /**
+     * Oauth 로그인 성공 시 인증 서버에서 액세스 토큰과 리프레시 토큰을 가져와서 쿠키로 만들고 응답에 넣는 역할을 수행하는 메서드
+     *
+     * @param request        요청
+     * @param response       응답
+     * @param authentication 인증 정보
+     * @throws IOException 입력 출력 예외
+     */
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         OAuth2AuthenticationToken oauth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
         String userName = capitalize(oauth2AuthenticationToken.getAuthorizedClientRegistrationId().concat("_").concat(authentication.getName()));
 
@@ -49,6 +62,12 @@ public class CustomOauth2SuccessHandler implements AuthenticationSuccessHandler 
         response.sendRedirect(frontUrl);
     }
 
+    /**
+     * provider의 첫 글자를 대문자로 바꿔주는 메서드
+     *
+     * @param inputString provider + 인증 name
+     * @return 변환된 이름
+     */
     private static String capitalize(String inputString) {
         String first = String.valueOf(inputString.charAt(0));
         String other = inputString.substring(1);
