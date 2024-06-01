d0309d28a53801a1042ae98d77abf61d4743e714
pass0210 <pass0210@gmail.com>
2024-06-01 17:44:14 +0900

    Docs: RefreshTokenResponse 수정
    JavaDoc 추가

--- src/main/java/com/user/management/dto/RefreshTokenResponse.java
+++ src/main/java/com/user/management/dto/RefreshTokenResponse.java
@@ -3,6 +3,12 @@ package com.user.management.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+/**
+ * 리프레쉬 토큰과 만료 시간을 저장하는 dto
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 @Getter
 @Setter
 @AllArgsConstructor
